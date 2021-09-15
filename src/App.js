27eb018606ad1f08bc12e3bdd5feb8b7a7341661
jehadabuawwad <jehadabuawwad@outlook.com>
2021-09-15 12:57:05 +0300

    Styling Done By Jehad

--- src/App.js
+++ src/App.js
@@ -36,48 +36,48 @@ class App extends React.Component {
   };
   render() {
     return (
-        <Router>
-          <Row style={{ marginTop: 0 }}>
-            <Col md={{ span: 6, offset: 3 }}>
-              <Header
-                loginHandler={this.loginHandler}
-                logoutHandler={this.logoutHandler}
-                user={this.state.user}
-                onLogout={this.logoutHandler}
-              />
-            </Col>
-          </Row>
-          <Switch>
-            <Route exact path='/'>
-              <Row>
-                <Col style={{ marginTop: 10 }} md={{ span: 4, offset: 3 }}>
-                  {<BestBooks />}
-                </Col>
-                <Col
-                  style={{ marginTop: 20,marginBottom:20 }}
-                  xs={{ span: 10,order: 'last', offset: 1 }}
-                  md={{ span: 2, offset: 0 }}
-                  sm={3}
-                >
-                  <LoginForm
-                    loginHandler={this.loginHandler}
-                    formSubmit={this.formSubmit}
-                    submitted={this.state.submitted}
-                    user={this.state.user}
-                  />
-                </Col>
-              </Row>
-            </Route>
-            <Route exact path='/profile'>
-              <Profile />
-            </Route>
-          </Switch>
-          <Row>
-            <Col   md={{ span: 6, offset: 3 }}>
-              <Footer />
-            </Col>
-          </Row>
-        </Router>
+      <Router>
+        <Row style={{ marginTop: 0 }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Header
+              loginHandler={this.loginHandler}
+              logoutHandler={this.logoutHandler}
+              user={this.state.user}
+              onLogout={this.logoutHandler}
+            />
+          </Col>
+        </Row>
+        <Switch>
+          <Route exact path='/'>
+            <Row>
+              <Col style={{ marginTop: 10 }} md={{ span: 4, offset: 3 }}>
+                {<BestBooks />}
+              </Col>
+              <Col
+                style={{ marginTop: 20, marginBottom: 20 }}
+                xs={{ span: 10, order: 'last', offset: 1 }}
+                md={{ span: 2, offset: 0 }}
+                sm={3}
+              >
+                <LoginForm
+                  loginHandler={this.loginHandler}
+                  formSubmit={this.formSubmit}
+                  submitted={this.state.submitted}
+                  user={this.state.user}
+                />
+              </Col>
+            </Row>
+          </Route>
+          <Route exact path='/profile'>
+            <Profile />
+          </Route>
+        </Switch>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Footer />
+          </Col>
+        </Row>
+      </Router>
     );
   }
 }
